0bcf8c2e6952df9b9e4011bba770171eb1236113
Georg Oeltzschner <schorschinho@users.noreply.github.com>
2026-04-09 14:49:31 -0400

    Add files required for Sphinx building

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,7 @@
-﻿project = "Osprey Documentation"
+﻿project = "Osprey"
+copyright = "2020-2026, Osprey Developers"
 author = "Osprey Developers"
+release = "3.0"
 
 extensions = []
 
@@ -8,5 +10,11 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 language = "en"
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+
+html_theme_options = {
+    "logo_only": False,
+    "display_version": True,
+    "navigation_depth": 3,
+}
